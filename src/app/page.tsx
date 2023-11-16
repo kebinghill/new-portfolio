7af8807ecc78fe587c99bcd3f1fb35f7918501a4
Kevin Gil <93kevingil@gmail.com>
2023-11-16 17:58:53 -0300

    redirect home to bio for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import Header from './components/Header';
+import { redirect } from 'next/navigation';
 
 const Home = () => {
+  redirect('/bio');
   return (
     <div className='min-w-[100vw]'>
       <Header />
