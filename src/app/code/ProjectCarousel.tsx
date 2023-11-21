06d0697500f4414b79b4703b997c73ef99b666b0
Kevin Gil <93kevingil@gmail.com>
2023-11-21 18:02:05 -0300

    fix carousel, fix window sizing and alignment

--- src/app/code/ProjectCarousel.tsx
+++ src/app/code/ProjectCarousel.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Carousel, Modal } from 'flowbite-react';
+import { Carousel } from 'flowbite-react';
 import Image from 'next/image';
 import ProjectModal from './ProjectModal';
 
@@ -14,7 +14,7 @@ const images = [
     width={1000}
     height={1000}
     alt='SLCTR'
-    className='aspect-[14/12] overflow-clip'
+    className='aspect-[14/12] flex h-full w-full items-center justify-center'
   />,
   <Image
     src='/portfolio-code.png'
@@ -22,29 +22,28 @@ const images = [
     width={1000}
     height={1000}
     alt=''
-    className='aspect-[14/12] overflow-clip'
+    className='aspect-[14/12] flex h-full w-full items-center justify-center'
   />,
 ];
 
 const titles = ['SLCTR APP', 'PORTFOLIO APP'];
 
-const descriptions = [];
+const descriptions = ['', ''];
 
 const ProjectCarousel = () => {
-  const [currentTitle, setCurrentTitle] = useState('');
-  const [currentDescription, setCurrentDescription] = useState('');
+  const [currentTitle, setCurrentTitle] = useState(titles[0]);
+  const [currentDescription, setCurrentDescription] = useState(descriptions[0]);
   const [openModal, setOpenModal] = useState(false);
 
   return (
     <div>
-      <div className='flex flex-col items-center'>
+      <div className='flex flex-col items-center border'>
         <div className='text-2xl font-extralight'>{currentTitle}</div>
-        <div className='w-[95vw] min-h-[200px] aspect-[14/12]  overflow-clip'>
+        <div className='flex justify-center items-center md:w-[700px] w-[95vw] aspect-[14/12]'>
           <Carousel
             slide={false}
-            className='overflow-clip'
             onSlideChange={(idx) => {
-              setCurrentTitle(titles[idx]);
+              return setCurrentTitle(titles[idx]);
             }}
           >
             {images.map((image) => (
